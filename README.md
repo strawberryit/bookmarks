ccff34065b574554985d6b98f2bc7c46f6993b22
Andy <strawberryit@daum.net>
2021-02-26 01:02:22 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Bookmarks
+* [Terraform](#docs/terraform.md)
+
+## Usage
+### Generate `README.md`
+```
+$ python3 build.py
+```
---
+++ build.py
@@ -0,0 +1,49 @@
+#-*- coding: utf-8 -*-
+import re
+import string
+import glob
+import os.path
+import io
+
+
+header = """
+# Bookmarks
+"""
+
+footer = """
+## Usage
+### Generate `README.md`
+```
+$ python3 build.py
+```
+"""
+
+def make_pretty_name(name):
+    pretty_name = re.sub(r'-', ' ', name)
+    return string.capwords(pretty_name)
+
+
+readme = io.open('README.md', 'r+', encoding='utf-8')
+readme.write(header)
+
+files = sorted(glob.glob('docs/**', recursive=True))
+documents = []
+directories = []
+
+for file in files:
+    if file.endswith('.md'):
+        documents.append(file)
+
+for document in documents:
+    title = re.sub('docs/|\.md', '', document).capitalize()
+    readme.write(f"* [{title}](#{document})\n")
+
+
+#for directory in directories:
+#    readme.write("\n## " + directory.capitalize() + "\n")
+#    sub_files = glob.glob(directory + '/*.md')
+#    for sub_file in sub_files:
+#        readme.write("* [" + make_pretty_name(os.path.basename(sub_file)) + "](" + sub_file + ")\n")
+
+readme.write(footer)
+readme.close()
